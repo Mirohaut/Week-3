2492681d8b1df027029dccda7743be07274cc5f2
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-04-05 23:09:40 +0300

    Add files via upload

---
+++ routes/userRoute.js
@@ -0,0 +1,18 @@
+'use strict';
+const { body} = require('express-validator');
+const express = require('express');
+const multer  = require('multer');
+const userController = require('../controllers/userController');
+const router = express.Router();
+
+router.get('/', userController.user_list_get);
+router.post('/',
+body('email').isEmail(),
+body('passwd').isLength({min: 5}),
+userController.user_create);
+
+router.get('/:id', userController.user_get_by_id);
+router.put('/:id', userController.user_update);
+router.delete('/:id', userController.user_delete);
+
+module.exports = router;
